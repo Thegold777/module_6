91b3c42e8bac768cbb9c71f69a058f0f62a202c9
Thegold777 <165582387+Thegold777@users.noreply.github.com>
2024-05-13 12:20:58 +0300

    new

--- index.js
+++ index.js
@@ -1,15 +1,57 @@
-const leftToRight = 'Довод'.toLowerCase();
-let rightToLeft = '';
-for (let i = leftToRight.length - 1; i >= 0; i -= 1) {
-    rightToLeft += leftToRight[i];
-}
 
-if (leftToRight === rightToLeft) {
-    console.log('Слово "Довод" является палиндромом');
 
-} else {
-    console.log('Слово "Довод" не является палиндромом');
+
+function check(text) {
+    const leftToRight = text.toLowerCase();
+    let rightToLeft = '';
+    for (let i = leftToRight.length - 1; i >= 0; i -= 1) {
+        rightToLeft += leftToRight[i];
+    }
+
+    if (leftToRight === rightToLeft) {
+        console.log('Слово "' + text + '" является палиндромом');
+
+    } else {
+        console.log('Слово "' + text + '" не является палиндромом');
+
+    }
+
+
+};
+
+check("Довод");
+check("Сантимент");
+
+
+
+const arr = [1, 2, 3, 1, 5, 4, 2, 3, 5, 'they', 'don\'t', 'know', 'that', 'we', 'know', 'that', 'they', 'know' ];
+const arrUniq = new Set()
+arr.forEach(element => {
+ arrUniq.add(element)   
+});
+console.log(Array.from(arrUniq));
+
+
+
+const promptText = prompt('Введите число');
+const arr2 = [];
+for(let i = 0; i <= Number(promptText); i++){
+    arr2.push(i)
 }
+console.log(arr2);
+
+
+const arr3 = ["x","0","x","0","x","0","x","0","x"];
+let xo = "";
+const ex=[2,5,8];
+arr3.forEach((value,index)=>{
+    xo+=value+" ";
+    if(ex.includes(index)){
+        console.log(xo);
+        xo ="";
+    }
+})
+
 
 
 
