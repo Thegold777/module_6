40095301d05314416911a4b85f024ccef17dc0ec
Thegold777 <165582387+Thegold777@users.noreply.github.com>
2024-05-17 16:27:20 +0300

    .

--- index.js
+++ index.js
@@ -73,4 +73,15 @@ console.log(arrValues2);
 
 
 
+function printInfo(name, age){
+    console.log(`Name: ${name}, Age: ${age} `)
+}
 
+const person = {
+    name: "Vasya",
+    age: 1000
+}
+
+let func = printInfo;
+
+func.call(printInfo, person.name, person.age)
