30dee41379481691a276f1998fe5c2b8b6200d84
Thegold777 <165582387+Thegold777@users.noreply.github.com>
2024-05-20 20:30:14 +0300

    .

--- Myindex.js
+++ Myindex.js
@@ -1,36 +1,66 @@
-// function printInfo(name, age){
-//     console.log(`Name: ${name}, Age: ${age} `)
-// }
+ // задание 7.7.1
+ 
+ function printInfo(name, age){
+    console.log(`Name: ${name}, Age: ${age} `)
+}
 
-// const person = {
-//     name: "Vasya",
-//     age: 1000
-// }
+const person = {
+    name: "Vasya",
+    age: 1000
+}
 
-// let function = printInfo;
+let func = printInfo;
 
-// function.call(printInfo, person.name, person.age)
-
-
-
-
-// function calculate(a, b, operator) {
-//     return operator(a, b);
-// }
-
-// const values = [2, 3, "+"];
-// const result = calculate.apply(values);
-// console.log(result);
+func.call(printInfo, person.name, person.age)
 
 
-const users [ 
-{age: 18 },
-{age: 20 },
-{age: 30}
+// задание 7.7.2
+
+ function calculate(a, b, operator) {
+     return operator(a, b);
+}
+
+ const values = [2, 3, "+"];
+ const result = calculate.apply(values);
+ console.log(result);
+
+//задание 7.7.3
+
+const users = [
+    { name: "Alisa", age: 16 },
+    { name: "Kirill", age: 20 },
+    { name: "Bob", age: 30 }
 ];
 
-const newUsers = 
+const newUsersOver18 = users.filter(user => user.age >= 18);
+const nameUsersOver18 = newUsersOver18.map(user => user.name);
+console.log(nameUsersOver18);
 
+
+// задание 7.7.4
+
+function setFullName(obj, name) {
+    obj.fullName = name;
+  }
+  const setPersonFullName = setFullName.bind(null, person);
+  setPersonFullName("John Smith");
+  console.log(person.fullName);
+
+
+
+
+
+
+// задание 7.7.5
+
+const arr = [1, 2, 3, 4, 1, 2, 5];
+function uniqueSorted(arr) {
+    const unique = arr.filter((item, index) => arr.indexOf(item) === index);
+    const sortAsc = unique.sort((a, b) => a - b);
+    return sortAsc;  
+}
+const uniqueSortedArr = uniqueSorted(arr);
+console.log(uniqueSortedArr);
 
 
 
